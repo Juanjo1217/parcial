95e70d7a0dc0d91b184588d3d82320990949c363
Ubuntu <jjaragon@oop-jjaragon.b200ocpmchqehipu4oipqd4ree.phxx.internal.cloudapp.net>
2022-09-13 14:06:18 +0000

    code

---
+++ carcasa.py
@@ -0,0 +1,26 @@
+class library:
+    def __init__(self, book: str) -> None:
+        pass
+
+    """
+    manejo de libros
+    """
+    def book_state(self):
+        pass
+
+    def add_book(self):
+        pass
+
+class user:
+    def __init__(self, book: str) -> None:
+        pass
+
+    def get_book(self, book: str):
+        pass
+
+    def return_book(self, book: str):
+        pass
+
+    def dysplay_book(self):
+        pass
+
